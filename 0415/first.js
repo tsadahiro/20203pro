f03a629ad1fedd1e4aa73663cec2a6e1da90a01b
Taizo Sadahiro <sadahiro@tsuda.ac.jp>
2020-04-15 14:10:19 +0900

    first lecture

---
+++ 0415/first.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+	message: 'ABC',
+	color: 'color:black;' //黒
+    },
+    methods:{
+	plus(){
+	    this.message += " AIUEO ";
+	},
+	changecolor(){
+	    this.color = 'color:red;'; //赤
+	}
+    }
+})
